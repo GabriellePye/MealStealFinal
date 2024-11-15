bca8cfd7b13b78e84e7d2f5422207f7858850661
GabriellePye <gabriellepye@rockborne.com>
2024-11-15 05:17:51 +0000

    if i was cat I'd meow all day

--- mealstealfinalfinal.py
+++ mealstealfinalfinal.py
@@ -93,7 +93,7 @@ st.markdown("""
 <style>
 /* background */
 .stApp {
-    background: url('https://giphy.com/gifs/food-tFUzn0xvrFv1CI69mD'); #-- still need to adjust
+    background: url('https://giphy.com/embed/tFUzn0xvrFv1CI69mD'); #-- still need to adjust
     background-size: cover; 
     background-position: top;
 }
